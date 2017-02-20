72709ce209bd853d30120f463e5b592d0de01a9a
Mofallo999 <ryanfarrah25410@gmail.com>
2017-02-20 11:30:10 +0100

    Add template/index, link to css, js, font. Include $page in main

---
+++ view/template/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE>
+
+
+<html>
+   
+   
+    
+<head>
+        
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.css" media="scrren,projection"/>
+    <link rel="stylesheet" src="view/css/style.css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono"> <!-- font-family: "Roboto Mono", monospace -->
+        
+    <meta charset="utf-8"/>
+    <meta name="description" content="Website for transfer files to email"/>
+        
+</head>
+
+
+
+<body>
+
+
+
+    <header>
+
+
+
+    </header>
+
+    <main>
+
+        <?php
+
+        include ($page);
+
+        ?>
+
+    </main>
+
+    <footer>
+
+
+    </footer>
+
+
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="view/js.js"></script>
+    
+
+</body>
+
+
+
+</html>
