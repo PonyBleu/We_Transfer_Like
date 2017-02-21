82c06b7ef3ea85e5b47118b0d51af4f4da55f907
Mofallo999 <ryanfarrah25410@gmail.com>
2017-02-21 11:02:26 +0100

    Navigation bar header 88%

--- view/js/animation.js
+++ view/js/animation.js
@@ -1,8 +1,8 @@
 $(".button-collapse").click(function()
 {
+    $()
     $("#logo").animate({
-    opacity: 0.25,
-    left: "+=50",
-    height: "toggle"};
-});
+  
+        marginLeft: "16em"
+    });
 });
--- view/template/index.php
+++ view/template/index.php
@@ -74,11 +74,9 @@ session_start();
         
         <ul id="slide-out" class="side-nav">
             <li>
-                <div class="userView">
-                    <div class="background"> <img src="images/office.jpg"> </div>
-                    <a href="#!user"><img class="circle" src="images/yuna.jpg"></a> <a href="#!name"><span class="white-text name">John Doe</span></a> <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a> </div>
+            
             </li>
-            <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+            <li><i class="material-icons"><img src="view/img/Outlook.png"></i><a href="#!">Outlook</a></li>
             <li><a href="#!">Second Link</a></li>
             <li>
                 <div class="divider"></div>
