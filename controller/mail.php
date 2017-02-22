2a4f712e5058566e1318b676f4a37f31adb035fb
Hugi Romane <romane.h@codeur.online>
2017-02-22 14:23:50 +0100

    cutsom mail

--- controller/mail.php
+++ controller/mail.php
@@ -6,8 +6,8 @@ if(isset($_POST['submit'])){
     $site = "Wetransfer Like <noreply@wetransferlike.com>";
     $subject = "$from vous a envoyé un fichier depuis Wetransfer Like !";
     $subject2 = "Copie de votre envoi depuis Wetransfer Like";
-    $message = $from . " " . " vous a transmis un fichier, pour le télécharger <a href='http://romaneh.marmier.codeur.online/We_Transfer_Like/view/download.php?url_fichier='>cliquez ici</a> " . "\n\n" . $_POST['message'];
-    $message2 = " Copie de votre message pour " . $to . "\n\n" . $_POST['message'];
+    $message = "<span style='color : #003752'><span style='font-size : 30px'>Wetransfer Like</span></span>" . "<br><br>" . $from  . "<br>" . "<span style='color : #0078b8'><span style='font-family : Helvetica'> Vous a transmis un fichier, pour le télécharger </span></span><a href='http://romaneh.marmier.codeur.online/We_Transfer_Like/view/download.php?url_fichier='><span style='color : #003752'><span style ='font-size : 20px'>cliquez ici</span></span></a> " . "\n\n" . $_POST['message'];
+    $message2 = "Copie de votre message pour" . $to . "\n\n";
     $headers = "From: \"Wetransfer Like\"<noreply@wetransferlike.com>\n";
     $headers .= "Content-Type: text/html; charset=\"utf-8\"";
     $headers2 = "From:" . $site;    
