05344399400bd83f3219b1b385262371281fc5a9
Mofallo999 <ryanfarrah25410@gmail.com>
2017-02-22 10:33:35 +0100

    Rabdom generate background-image

--- view/js/animation.js
+++ view/js/animation.js
@@ -1,4 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function() 
+{
+    
+    var images=["view/img/background1.jpg",
+               "view/img/background2.jpg"];
+    
+//    $("body").css({"background-image": "url(view/img/background1.jpg)"});
+    
+    var randomImg = Math.floor(Math.random() * images.length);
+    
+    var bgImg = 'url(' + images[randomImg] + ')';
+    
+    $("body").css({"background-image" :bgImg, "background-size": "cover"});
+    
     $(".button-collapse").click(function()
     {
         $()
