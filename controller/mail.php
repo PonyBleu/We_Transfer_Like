03bb4e2b8122e3096c8ac6e6f5acdd3b0a2d221c
Hugi Romane <romane.h@codeur.online>
2017-02-22 14:39:36 +0100

    Custom copie mail

--- controller/mail.php
+++ controller/mail.php
@@ -7,12 +7,12 @@ if(isset($_POST['submit'])){
     $subject = "$from vous a envoyé un fichier depuis Wetransfer Like !";
     $subject2 = "Copie de votre envoi depuis Wetransfer Like";
     $message = "<span style='color : #003752'><span style='font-size : 30px'>Wetransfer Like</span></span>" . "<br><br>" . $from  . "<br>" . "<span style='color : #0078b8'><span style='font-family : Helvetica'> Vous a transmis un fichier, pour le télécharger </span></span><a href='http://romaneh.marmier.codeur.online/We_Transfer_Like/view/download.php?url_fichier='><span style='color : #003752'><span style ='font-size : 20px'>cliquez ici</span></span></a> " . "\n\n" . $_POST['message'];
-    $message2 = "Copie de votre message pour" . $to . "\n\n";
+    $message2 = "<span style='color : #003752'><span style='font-size : 30px'>Wetransfer Like</span></span>" . "<br><br>" . "Copie de votre message pour : " . "<br>" . $to . "<br>" . $_POST['message'] . "<span style='color : #0078b8'><span style='font-family : Helvetica'> Vous avez transmis un fichier, pour le télécharger </span></span><a href='http://romaneh.marmier.codeur.online/We_Transfer_Like/view/download.php?url_fichier='><span style='color : #003752'><span style ='font-size : 20px'>cliquez ici</span></span></a> ";
     $headers = "From: \"Wetransfer Like\"<noreply@wetransferlike.com>\n";
     $headers .= "Content-Type: text/html; charset=\"utf-8\"";
     $headers2 = "From:" . $site;    
     mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); 
+    mail($from,$subject2,$message2,$headers); 
   
     }
 ?>
