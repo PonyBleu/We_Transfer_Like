6125c61453448a85669e364aefd6e3dc585c64c0
Unknown <cyril-ix@live.fr>
2017-02-27 15:34:34 +0100

    Debug template

--- view/js/animation.js
+++ view/js/animation.js
@@ -1,62 +1,62 @@
 
-$(document).ready(function() 
+$(document).ready(function()
 {
-    
+
     var outsideBar = $("#slide-out");
-    
+
     var images=["view/img/background1.jpg",
                "view/img/background2.jpg"];
-    
+
     var randomImg = Math.floor(Math.random() * images.length);
-    
+
     var bgImg = 'url(' + images[randomImg] + ')';
-    
+
     $("body").css({"background-image" :bgImg, "background-size": "cover"});
-    
-    $("button").click(function () 
+
+    $("button").click(function ()
     {
         animateFinger();
-        
+
         function animateFinger()
         {
-        
+
         $("#finger").animate({
             opacity: 0.25,
             marginRight: "3em"
-        },  
-                             
+        },
+
         {
             duration: 750
-            
+
         }), 100,  $("#finger").animate({
-            
+
             opacity: 1,
             marginRight: "0em"
-        }, 
+        },
             {
-            
-            duration: 750 
-            
+
+            duration: 750
+
             }), 100, $("#finger").animate({
-            
+
             opacity: 1
-            
+
         }, 0, function(){
-            
+
             animateFinger();
         });
-            
+
         }
     });
-                            
-    
-    
+
+
+
 //    if($("#finger").css("display") == "block")
 //        {
 //            console.log("Helle World");
 //        }
 //    else{
-//        
+//
 //    }
 //    $("#finger").animate
 //    ({
@@ -64,8 +64,8 @@ $(document).ready(function()
 //        left: "+=50"
 //    });
 
-    
-    
+
+
     $(".button-collapse").click(function()
     {
         $("#logo").animate({
@@ -73,92 +73,92 @@ $(document).ready(function()
             marginLeft: "16em"
         });
     });
-    
-    
+
+
       $("body").click(function (e)
         {
-          
+
           if (outsideBar.is(e.target))
             {
-                  
+
                 $("#logo").animate({
-                marginLeft: "16em" 
-                }); 
-                  
+                marginLeft: "16em"
+                });
+
             }
-          
+
           else
           {
               $("#logo").animate({
-              marginLeft: "3em" 
-              });  
+              marginLeft: "3em"
+              });
           }
 
         });
-    
 
-    
-        
+
+
+
 });
 
-$(document).ready(function() 
+$(document).ready(function()
 {
-    
+
     var outsideBar = $("#slide-out");
-    
+
     var images=["view/img/background1.jpg",
                "view/img/background2.jpg"];
-    
+
     var randomImg = Math.floor(Math.random() * images.length);
-    
+
     var bgImg = 'url(' + images[randomImg] + ')';
-    
+
     $("body").css({"background-image" :bgImg, "background-size": "cover"});
-    
-    $("button").click(function () 
+
+    $("button").click(function ()
     {
         animateFinger();
-        
+
         function animateFinger()
         {
-        
+
         $("#finger").animate({
             opacity: 0.25,
             marginRight: "3em"
-        },  
-                             
+        },
+
         {
             duration: 750
-            
+
         }), 100,  $("#finger").animate({
-            
+
             opacity: 1,
             marginRight: "0em"
-        }, 
+        },
             {
-            
-            duration: 750 
-            
+
+            duration: 750
+
             }), 100, $("#finger").animate({
-            
+
             opacity: 1
-            
+
         }, 0, function(){
-            
+
             animateFinger();
         });
-            
+
         }
     });
-                            
-    
-    
+
+
+
 //    if($("#finger").css("display") == "block")
 //        {
 //            console.log("Helle World");
 //        }
 //    else{
-//        
+//
 //    }
 //    $("#finger").animate
 //    ({
@@ -166,8 +166,8 @@ $(document).ready(function()
 //        left: "+=50"
 //    });
 
-    
-    
+
+
     $(".button-collapse").click(function()
     {
         $("#logo").animate({
@@ -175,32 +175,32 @@ $(document).ready(function()
             marginLeft: "16em"
         });
     });
-    
-    
+
+
       $("body").click(function (e)
         {
-          
+
           if (outsideBar.is(e.target))
             {
-                  
+
                 $("#logo").animate({
-                marginLeft: "16em" 
-                }); 
-                  
+                marginLeft: "16em"
+                });
+
             }
-          
+
           else
           {
               $("#logo").animate({
-              marginLeft: "3em" 
-              });  
+              marginLeft: "3em"
+              });
           }
 
         });
-    
 
-    
-        
+
+
+
 });
 $(document).ready(function()
 {
